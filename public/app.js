38c650285711704c3fdadc725eb9d9a214cb6360
dsuskauer <34040672+dsuskauer@users.noreply.github.com>
2018-12-02 20:53:25 -0500

    Add files via upload
    Most current version. Functionality is there. Need to make it look good now.

---
+++ public/app.js
@@ -0,0 +1,355 @@
+const firestore = firebase.firestore();
+const settings = {/* your settings... */ timestampsInSnapshots: true};
+firestore.settings(settings);
+var db = firebase.firestore();
+
+
+function getURL() {
+    var urlInfo = window.location.hash.substring(1);
+
+    var url = urlInfo.split("#", 1);
+
+    return url;
+}
+
+//meal input to database
+function addMeal() {
+
+    var date = document.getElementById("mealInput")[0].value;
+    var descriptionOfMeal = document.getElementById("mealInput")[1].value;
+    var costOfMeal = document.getElementById("mealInput") [2].value;
+
+    db.collection("Meal").doc(date).set({
+        description: descriptionOfMeal,
+        cost: costOfMeal,
+        numberUsers: 0
+
+    })
+        .then(function () {
+            console.log("Document successfully written!");
+            // window.location.href='Summary.html' + '#' + name;
+        })
+        .catch(function (error) {
+            console.error("Error writing document: ", error);
+        });
+
+}
+
+//function to add volunteers to database
+function addVolunteer() {
+
+    var username = document.getElementById("volunteerInput")[0].value;
+    var name = document.getElementById("volunteerInput")[1].value;
+    var hoursVolunteered = document.getElementById("volunteerInput")[2].value;
+    var milesDriven = document.getElementById("volunteerInput")[3].value;
+
+    db.collection("volunteers").doc(username).set({
+        username: username,
+        name: name,
+        hoursVolunteered: hoursVolunteered,
+        milesDriven: milesDriven
+    })
+        .then(function () {
+            console.log("Document successfully written!");
+            // window.location.href='Summary.html' + '#' + name;
+        })
+        .catch(function (error) {
+            console.error("Error writing document: ", error);
+        });
+
+}
+
+
+var arr = [];
+
+//creates an array of all the activities on the page
+var arr_activities = [
+    "Ping-Pong",
+    "Billiards",
+    "Gym",
+    "Dominoes",
+    "Bridge",
+    "Bingo",
+    "Yoga",
+    "Art Therapy",
+    "Ceramics",
+    "Movie",
+    "Library",
+    "Puzzles",
+    "Computer",
+    "Zumba",
+    "Special Event"
+]
+
+//this function looks to see if each checkbox is checked or not, and then either adds or
+// removes from array to pass to database. modified from code on stackoverflow
+function addPrior(e) {
+    {
+        // if check
+        if (e.target.checked) {
+            addToArray(e.target);
+        } else {
+            removeToArray(e.target);
+
+        }
+
+        console.log(arr);
+    }
+
+//this function adds to the array to pass to the database, called by addPrior
+    function addToArray(obj) {
+        var object = obj.value;
+        arr.push(object);
+    }
+
+}
+
+//this function removes from the array, which is called by addPrior if the checkbox is not checked
+function removeToArray(obj) {
+    var index = arr.indexOf(obj.value);
+    if (index > -1) {
+        arr.splice(index, 1);
+    }
+    console.log(arr);
+}
+
+//this function looks at the existing array of activities and instantiates a new array to hold
+//the values of the activities on the page with their appropriate index location
+function getAddActivity() {
+    arr_activities;
+    var array = arr;
+    var newArr = [];
+    for (var i = 0; i < array.length; i++) {
+        newArr.push(arr_activities[array[i]])
+    }
+  addActivity(newArr);
+    updateActivity(newArr);
+}
+
+
+function addActivity(newArr) {
+    var date = currentDate();
+    var username = String(getURL());
+    var activity = newArr;
+
+    console.log(activity);
+
+    db.collection("Users").doc(username).collection("Activities").doc(date).set({
+           Activity: activity
+    })
+        .then(function () {
+            console.log("Document successfully written!");
+            // window.location.href='Summary.html' + '#' + name;
+        })
+        .catch(function (error) {
+            console.error("Error writing document: ", error);
+        }); console.log("here");
+        document.write("You successfully Submitted Your Activities for Today")
+}
+
+//function to update activities to main activities collection
+function updateActivity(newArr){
+     var date = currentDate();
+     var newArr = newArr;
+     var username = String (getURL());
+
+     for(var i =0; i<newArr.length; i++){
+         db.collection("Activities").doc(newArr[i]).collection(date).doc(username).set({
+
+         })
+             .then(function () {
+                 console.log("Document successfully written!");
+                 // window.location.href='Summary.html' + '#' + name;
+             })
+             .catch(function (error) {
+                 console.error("Error writing document: ", error);
+             });
+     }
+}
+
+//get current date, sourced from stackoverflow
+function currentDate() {
+    var today = new Date();
+    var dd = today.getDate();
+    var mm = today.getMonth() + 1; //January is 0!
+    var yyyy = today.getFullYear();
+
+    if (dd < 10) {
+        dd = '0' + dd
+    }
+
+    if (mm < 10) {
+        mm = '0' + mm
+    }
+
+    today = mm + '-' + dd + '-' + yyyy;
+    return (today);
+}
+
+
+
+
+// meal output from database
+function getDescription() {
+    var date = currentDate();
+
+    var docRef = db.collection("Meal").doc(date);
+
+
+    docRef.get().then(function (doc) {
+        if (doc.exists) {
+            console.log("Document data:", doc.data());
+            var test = String(doc.data().description);
+            localStorage.setItem(("test"), test);
+        } else {
+// doc.data() will be undefined in this case
+            console.log("No such document!");
+        }
+    }).catch(function (error) {
+        console.log("Error getting document:", error);
+    });
+    var test2 = localStorage.getItem("test");
+    document.write(test2);
+    // return test2;
+}
+
+//function to show the menu on current page of cafeteria menu
+
+function getMenu() {
+    var date = currentDate();
+
+    var docRef = db.collection("Meal").doc(date);
+
+
+    docRef.get().then(function (doc) {
+        if (doc.exists) {
+            console.log("Document data:", doc.data());
+            var test = String(doc.data().description);
+            localStorage.setItem(("test"), test);
+        } else {
+// doc.data() will be undefined in this case
+            console.log("No such document!");
+        }
+    }).catch(function (error) {
+        console.log("Error getting document:", error);
+    });
+    var test2 = localStorage.getItem("test");
+    return (test2);
+    // return test2;
+}
+
+function transmitName() {
+    var username = document.getElementById("searchVolunteerInfo")[0].value;
+    window.location.href = 'AdminResults.html' + '#' + username;
+
+}
+
+//get volunteer info helper method
+function getVolunteerInfo() {
+    var getURLName = String(getURL());
+
+    db.collection("volunteers").where("username", "==", getURLName).get().then(function (querySnapshot) {
+        querySnapshot.forEach(function (doc) {
+            // doc.data() is never undefined for query doc snapshots
+            localStorage.setItem("hoursVolunteered", doc.data().hoursVolunteered);
+            localStorage.setItem("milesDriven", doc.data().milesDriven);
+            localStorage.setItem("name", doc.data().name);
+
+
+        });
+    });
+
+    var hoursVolunteered = localStorage.getItem("hoursVolunteered");
+    var milesDriven = localStorage.getItem("milesDriven");
+    var name = localStorage.getItem("name");
+
+    var array = [name, milesDriven, hoursVolunteered];
+    console.log(array);
+    console.log(getURLName);
+    return array;
+
+
+}
+
+function addUser(){
+    var date = currentDate();
+    var name = "test";
+    console.log("testing2");
+db.collection("Users").doc(name).collection("Plan").doc(date).set({
+Food: date
+
+})
+    .then(function() {
+        console.log("Document successfully written!");
+    })
+    .catch(function(error) {
+        console.error("Error writing document: ", error);
+    });
+
+}
+
+// Activity output from database
+function getActivities() {
+    var date = document.getElementById("searchUserActivities")[0].value;
+    var desiredActivity=document.getElementById("searchUserActivities")[1].value;
+
+    db.collection("Activities").doc(desiredActivity).collection(date).get().then(function(querySnapshot) {
+        querySnapshot.forEach(function(doc) {
+
+            console.log(doc.id, " => ", doc.data());
+        });
+    });
+    document.write("2");
+}
+
+function addUser() {
+
+    var username = document.getElementById("registration")[0].value;
+    var password = document.getElementById("registration")[1].value;
+    var name = document.getElementById("registration")[2].value;
+    var age = document.getElementById("registration")[3].value;
+
+
+    db.collection("Users").doc(username).set({
+        username: username,
+        name: name,
+        age: age,
+        password: password
+    })
+        .then(function () {
+            console.log("Document successfully written!");
+
+            window.location.href= "Activities.html" + '#'+ username;
+        })
+        .catch(function (error) {
+            console.error("Error writing document: ", error);
+        });
+
+
+}
+
+
+
+function loginPage(){
+    var username = document.getElementById("signIn")[0].value;
+    var password = document.getElementById("passWord")[1].value;
+    window.location.href="Activities.html"+'#'+ username;
+
+}
+//
+//     docRef.get().then(function (doc) {
+//         if (doc.exists) {
+//             console.log("Document data:", doc.data());
+//             var test = String(doc.data().description);
+//             localStorage.setItem(("test"), test);
+//         } else {
+// // doc.data() will be undefined in this case
+//             console.log("No such document!");
+//         }
+//     }).catch(function (error) {
+//         console.log("Error getting document:", error);
+//     });
+//     var test2 = localStorage.getItem("test");
+//     document.write(test2);
+//     // return test2;
+// }
